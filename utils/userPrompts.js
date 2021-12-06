01b4ea12cb8c778235ee08df051acb25ebb65b2d
gabriel-perez1 <g.perez.carbo@gmail.com>
2021-12-06 16:37:18 -0500

    final user prompts

--- utils/userPrompts.js
+++ utils/userPrompts.js
@@ -85,5 +85,41 @@ const projectPrompts = () => {
 				}
 			}
 		},
+
+		{
+      type: 'checkbox',
+      name: 'Licenses',
+      message: 'Please select the license terms the project is licensed under',
+      choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-2-Clause', 'BSD-3-Clause', 'BSD-4-Clause']
+    },
+
+		{
+			type: 'input',
+			name: 'Github',
+			message: 'Enter your Github username',
+			validate: testInput => {
+				if (testInput) {
+					return true;
+				} else {
+					console.log('Please enter your Github username');
+					return false;
+				}
+			}
+		},
+
+		{
+			type: 'input',
+			name: 'Email',
+			message: 'Enter your email',
+			validate: testInput => {
+				if (testInput) {
+					return true;
+				} else {
+					console.log('Please enter your email');
+					return false;
+				}
+			}
+		},
+
 	])
 }
