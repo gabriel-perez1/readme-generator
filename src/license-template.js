80898d10fb67cf565ab1be5a76b485c0041cbb70
gabriel-perez1 <g.perez.carbo@gmail.com>
2021-12-13 20:56:21 -0500

    dot notation switch statement

--- src/license-template.js
+++ src/license-template.js
@@ -1,49 +1,37 @@
-const licenseChoice = (answers) => {
+const licenseChoice = answers => {
+	let response = answers.license;
+	switch (response) {
+		case 'MIT':
+				answers.licenseText = 'Distributed under the [MIT license](https://opensource.org/licenses/MIT';
+				answers.licenseLogo = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+			break;
 
-switch (answers.licenses) {
-	case 'MIT':
-		answers += {
-			licenseText: 'Distributed under the [MIT license](https://opensource.org/licenses/MIT',
-			licenseLogo: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-			}
+		case 'Apache-2.0':
+				answers.licenseText = 'Distributed under the [Apache-2.0 license](https://opensource.org/licenses/Apache-2.0)';
+				answers.licenseLogo = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
 		break;
 
-	case 'Apache-2.0':
-		answers += {
-			licenseText: 'Distributed under the [Apache-2.0 license](https://opensource.org/licenses/Apache-2.0)',
-			licenseLogo: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-			}
-	break;
+		case 'GPL-3.0':
+				answers.licenseText = 'Distributed under the [GPL-3.0 license](https://www.gnu.org/licenses/gpl-3.0)';
+				answers.licenseLogo = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+		break;
 
-	case 'GPL-3.0':
-		answers += {
-			licenseText: 'Distributed under the [GPL-3.0 license](https://www.gnu.org/licenses/gpl-3.0)',
-			licenseLogo: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-			}
-	break;
+		case 'BSD-2-Clause':
+				answers.licenseText = 'Distributed under the [BSD-2-Clause license](https://opensource.org/licenses/BSD-2-Clause)';
+				answers.licenseLogo = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+		break;
 
-	case 'BSD-2-Clause':
-		answers += {
-			licenseText: 'Distributed under the [BSD-2-Clause license](https://opensource.org/licenses/BSD-2-Clause)',
-			licenseLogo: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-			}
-	break;
+		case 'BSD-3-Clause':
+				answers.licenseText = 'Distributed under the [BSD-3-Clause license](https://opensource.org/licenses/BSD-3-Clause)';
+				answers.licenseLogo = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+		break;
 
-	case 'BSD-3-Clause':
-		answers += {
-			licenseText: 'Distributed under the [BSD-3-Clause license](https://opensource.org/licenses/BSD-3-Clause)',
-			licenseLogo: '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
-			}
-	break;
-
-	case 'Zero-Clause BSD':
-		answers += {
-			licenseText: 'Distributed under the [Zero-Clause BSD license](https://opensource.org/licenses/0BSD)',
-			licenseLogo: '[![License](https://img.shields.io/badge/License-0_BSD--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
-			}
-	break;
+		case 'Zero-Clause BSD':
+				answers.licenseText = 'Distributed under the [Zero-Clause BSD license](https://opensource.org/licenses/0BSD)';
+				answers.licenseLogo = '[![License](https://img.shields.io/badge/License-0_BSD--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+		break;
+		}
 	}
-}
 
 module.exports = licenseChoice;
 
--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
+const licenseChoice = require('../src/license-template');
 const generateLicense = require('../src/license-template');
 const generatePage = require('../src/md-template');
 const writeFile = require('../utils/generate-md');
-const testAnswers = require ('./test-answer')
+const testAnswers = require ('./test-answer');
 
+/*
 writeFile(generatePage(testAnswers))
 	.then(res => {
 		console.log(res);
@@ -10,3 +12,15 @@ writeFile(generatePage(testAnswers))
 	.catch(err => {
 		console.log(err);
 	})
+*/
+
+licenseChoice(testAnswers)
+	.then(writeFile(generatePage(testAnswers)))
+		.then(res => {
+			console.log(res);
+		})
+		.catch(err => {
+			console.log(err);
+		})
+
+	
